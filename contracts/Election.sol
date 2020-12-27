eeacde2d38850254d9713a60b3924593a2a38b32
Volkan <volkanarisli@gmail.com>
2020-12-27 14:58:52 +0300

    Frontend added votin logic done

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,13 +1,49 @@
 //"SPDX-License-Identifier: UNLICENSED"
-pragma solidity *;
+pragma solidity >=0.5.16 <0.7.0;
 
 contract Election {
-    //Store candidate
-    //Read candidate
-    string public candidate;
+    // Model a Candidate
+    struct Candidate {
+        uint256 id;
+        string name;
+        uint256 voteCount;
+    }
 
-    //Constructor
-    function ElectionConst() public {
-        candidate = 'Candidate 1';
+    // Store accounts that have voted
+    mapping(address => bool) public voters;
+    // Store Candidates
+    // Fetch Candidate
+    mapping(uint256 => Candidate) public candidates;
+    // Store Candidates Count
+    uint256 public candidatesCount;
+
+    // voted event
+    event votedEvent(uint256 indexed _candidateId);
+
+    constructor() public {
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+
+    function addCandidate(string memory _name) private {
+        candidatesCount++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+    function vote(uint256 _candidateId) public {
+        // require that they haven't voted before
+        require(!voters[msg.sender]);
+
+        // require a valid candidate
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
+
+        // record that voter has voted
+        voters[msg.sender] = true;
+
+        // update candidate vote Count
+        candidates[_candidateId].voteCount++;
+
+        // trigger voted event
+        emit votedEvent(_candidateId);
     }
 }
